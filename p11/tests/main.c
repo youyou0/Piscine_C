d7456edaefac075e46da06603dc365f968482365
Youness Hamdi <youness.hamdi@zoho.com>
2018-06-16 22:11:00 +0200

    add unit test revlist

--- p11/tests/main.c
+++ p11/tests/main.c
@@ -1,7 +1,20 @@
 # include <stdlib.h>
 # include "my.h"
+# include "mylist.h"
 
-int	main()
+int	main(int ac, char **av)
 {
+  linked_list_t *list;
+
+  list = my_params_to_list(ac, av);
+  my_rev_list(&list);
+
+  while (list)
+    {
+      my_putstr(list->data);
+      my_putchar('\n');
+      list = list->next;
+    }
+
   return (0);
 }
