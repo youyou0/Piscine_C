bbcf3e87baa69da98ad6d320b7cbc7e5b734a303
Youness Hamdi <youness.hamdi@zoho.com>
2018-06-24 21:19:05 +0200

    add quick sort word array

--- p11/lib/my/my_sort_word_array.c
+++ p11/lib/my/my_sort_word_array.c
@@ -1,30 +1,62 @@
 # include "my.h"
 
-int	my_sort_word_array(char **tab)
+int	my_word_array_size(char **tab)
 {
-  int	smaller_str;
-  char	*tmp;
   int	i;
-  int	j;
 
   i = 0;
-  smaller_str = 0;
   while (tab[i])
     {
-      j = i;
-      while (tab[j])
-	{
-	  if (my_strcmp(tab[j], tab[smaller_str]) < 0)
-	    {
-	      smaller_str = j;
-	    }
-	  j += 1;
-	}
-      tmp = tab[smaller_str];
-      tab[smaller_str] = tab[i];
-      tab[i] = tmp;
       i += 1;
-      smaller_str = i;
     }
+  return (i);
+}
+
+char	**my_word_swap(char **word_tab, int pos, int pos2)
+{
+  char	*tmp;
+
+  tmp = word_tab[pos];
+  word_tab[pos] = word_tab[pos2];
+  word_tab[pos2] = tmp;
+  return (word_tab);
+}
+
+int	partition(char **word_tab, int start, int end)
+{
+  int	wall;
+  int	pivot;
+  int	i;
+
+  i = start;
+  pivot = end;
+  wall = start;
+  while (i < pivot)
+    {
+      if (my_strcmp(word_tab[i], word_tab[pivot]) < 0)
+	{
+	  word_tab = my_word_swap(word_tab, wall, i);
+	  wall += 1;
+	}
+      i += 1;
+    }
+  word_tab = my_word_swap(word_tab, wall, pivot);
+  return (wall);
+}
+
+char	**my_qsort_word_array(char **word_tab, int pivot, int end)
+{
+  if (pivot < end)
+    {
+      pivot = partition(word_tab, pivot, end);
+      word_tab = my_qsort_word_array(word_tab, 0, pivot - 1);
+      word_tab = my_qsort_word_array(word_tab, pivot + 1, end);
+    }
+  return (word_tab);
+}
+
+int	my_sort_word_array(char **tab)
+{
+  tab = my_qsort_word_array(tab, 0, my_word_array_size(tab) - 1);
   return (0);
 }
