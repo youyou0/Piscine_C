5fe4c2487876d3980f7051e7b9df67a8305382ff
Youness Hamdi <youness.hamdi@zoho.com>
2018-06-24 21:20:16 +0200

    unit test sort list

--- p11/tests/main.c
+++ p11/tests/main.c
@@ -2,19 +2,26 @@
 # include "my.h"
 # include "mylist.h"
 
-int	main(int ac, char **av)
+void		print_list(linked_list_t *list)
 {
-  linked_list_t *list;
-
-  list = my_params_to_list(ac, av);
-  my_rev_list(&list);
-
   while (list)
     {
       my_putstr(list->data);
       my_putchar('\n');
       list = list->next;
     }
+}
 
+int		main(int ac, char **av)
+{
+  linked_list_t *list;
+
+  list = my_params_to_list(ac, av);
+  //my_rev_list(&list);
+  //my_delete_nodes(&list, "hello", my_strcmp);
+  //my_concat_list(&list, &list);
+  print_list(list);
+  my_sort_list(&list, my_strcmp);
+  print_list(list);
   return (0);
 }
