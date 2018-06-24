7b99437bf56930de8282b2c9c004cd08a3c481ba
Youness Hamdi <youness.hamdi@zoho.com>
2018-06-24 21:19:37 +0200

    add list prototypes

--- p11/include/mylist.h
+++ p11/include/mylist.h
@@ -10,5 +10,16 @@ typedef struct		linked_list
 linked_list_t	*my_params_to_list(int ac, char *const *av);
 int		my_list_size(linked_list_t const *begin);
 void		my_rev_list(linked_list_t **begin);
+int		my_apply_on_nodes(linked_list_t *begin,
+				  int (*f)(void *));
+int		my_apply_on_matching_nodes(linked_list_t *begin,
+					   int (*f)(),
+					   void const *data_ref,
+					   int (*cmp)());
+int		my_delete_nodes(linked_list_t **begin,
+				void const *data_ref, int (*cmp)());
+void		my_concat_list(linked_list_t **begin1,
+			       linked_list_t **begin2);
+void		my_sort_list(linked_list_t **begin, int (*cmp)());
 
 #endif /* _MYLIST_H_ */
