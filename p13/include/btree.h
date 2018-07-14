a82ff5ef2bc79c0993038054983099b82a78cb4c
Youness Hamdi <youness.hamdi@zoho.com>
2018-07-14 18:04:17 +0200

    add search item prototype

--- p13/include/btree.h
+++ p13/include/btree.h
@@ -13,5 +13,7 @@ void		btree_apply_prefix(btree_t *root, int (*applyf)(void *));
 void		btree_apply_infix(btree_t *root, int (*applyf)(void *));
 void		btree_apply_suffix(btree_t *root, int (*applyf)(void *));
 void		btree_insert_data(btree_t **root, void *item, int (*cmpf)());
+void		*btree_search_item(btree_t const *root,
+				   void const *data_ref, int (*cmpf)());
 
 # endif /* _BTREE_H_ */
