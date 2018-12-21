6c75056c9570bdfcdea3bbbac773f285badfdcb3
Youness Hamdi <youness.hamdi@zoho.com>
2018-12-21 03:49:33 +0100

    update btree test

--- p13/tests/main.c
+++ p13/tests/main.c
@@ -8,15 +8,19 @@ int		main()
   btree_t	*tree;
 
   tree = NULL;
-  btree_insert_data(&tree, "hello", my_strcmp);
-  btree_insert_data(&tree, "world", my_strcmp);
-  btree_insert_data(&tree, "my", my_strcmp);
-  btree_insert_data(&tree, "name", my_strcmp);
-  btree_insert_data(&tree, "is", my_strcmp);
-  btree_insert_data(&tree, "youness", my_strcmp);
-  btree_apply_infix(tree, my_putstr);
-  my_putchar('\n');
-  my_putstr(btree_search_item(tree, "is", my_strcmp));
-  my_putchar('\n');
+  btree_insert_data(&tree, "Good", my_strcmp);
+  btree_insert_data(&tree, "luck", my_strcmp);
+  btree_insert_data(&tree, "for", my_strcmp);
+  btree_insert_data(&tree, "this", my_strcmp);
+  btree_insert_data(&tree, "great", my_strcmp);
+  btree_insert_data(&tree, "day", my_strcmp);
+  btree_insert_data(&tree, "of", my_strcmp);
+  btree_insert_data(&tree, "C", my_strcmp);
+  btree_insert_data(&tree, "pool", my_strcmp);
+  //btree_apply_infix(tree, my_putstr);
+  //my_putchar('\n');
+  //my_putstr(btree_search_item(tree, "this", my_strcmp));
+  //my_putchar('\n');
+  my_put_nbr(btree_level_count(tree));
   return (EXIT_SUCCESS);
 }
